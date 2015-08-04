c70c0e19a7c8ec19c2c442805874f2baaba458a2
Sergey <serg290696@gmail.com>
2015-08-04 16:21:21 +0300

    make it better

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println("exam. . .");
+        System.out.println("exam. . .aasd");
     }
 
     @Override
--- target/SampleHeroku-1.0-SNAPSHOT/index.jsp
+++ target/SampleHeroku-1.0-SNAPSHOT/index.jsp
@@ -12,6 +12,5 @@
 </head>
 <body>
 Heroku is behind you. . .
-New message!
 </body>
 </html>
